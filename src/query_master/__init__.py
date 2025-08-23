5fa36858475e81f35f863686b42bc798a76eb118
Sam Stone <51040430+samstone512@users.noreply.github.com>
2025-08-23 22:03:52 +0330

    Delete src/query_master directory

--- src/query_master/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/query_master/__init__.py
-# This makes the 'query_master' directory a Python package
-# and exposes the QueryMaster class for easy importing.
-
-from .rag_pipeline import QueryMaster
-
